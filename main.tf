633bbdc338ddfadff35a0d6ac527f3397245af05
Justin King <justin.king@speridian.com>
2022-11-10 17:03:15 -0800

    use new opional attribute in 1.3

--- main.tf
+++ main.tf
@@ -1,35 +1,18 @@
 ######
-# locals allow me to organize and merge values 
-# and create flexible objects with optional
-# attributes via merge() for applying settings.
-# This may be redone when terraform adds a 
-# native optional feature (one is in preview)
+# locals is a standard pattern I use to allow me to manipulate
+# or create new variables in one spot for use. On simple modules
+# I may just "pass-thru" vars to locals for consistency
 ######
 
 locals {
-  defaults = {
-    network = {
-      address_spaces     = []
-      dns_servers        = []
-    }
-    ngw_settings = {
-      public_ip_allocation_method = "Static"
-      public_ip_sku               = "Standard"
-      ngw_sku                     = "Standard"
-      idle_timeout_in_minutes     = "10"
-    }
-  }
-
-  # merge the default vlaues with varaibles here for a dynamic list of settings:
-  network           = merge(local.defaults.network, var.network)
-  ngw_settings      = merge(local.defaults.ngw_settings, var.ngw_settings)
-
   # these values are just mapped to local for consistency and have no transformation
-  global_settings                                       = var.global_settings
-  ngw_subnet_azs                                        = var.ngw_subnet_azs
-  subnet_service_endpoints                              = var.subnet_service_endpoints
-  private_endpoint_network_policies_enabled             = var.private_endpoint_network_policies_enabled
-  private_link_service_network_policies_enabled         = var.private_link_service_network_policies_enabled
-  subnets                                               = var.subnets
-  tags                                                  = var.tags
+  global_settings                               = var.global_settings
+  network                                       = var.network
+  ngw_settings                                  = var.ngw_settings
+  ngw_subnet_azs                                = var.ngw_subnet_azs
+  private_endpoint_network_policies_enabled     = var.private_endpoint_network_policies_enabled
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
+  subnet_service_endpoints                      = var.subnet_service_endpoints
+  subnets                                       = var.subnets
+  tags                                          = var.tags
 }
